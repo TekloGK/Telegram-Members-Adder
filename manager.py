1bf3e1d19d04293ce987b5b0ac51702a93aac51c
TekloGK <154121704+TekloGK@users.noreply.github.com>
2024-09-17 23:39:14 +0530

    Update manager.py

--- manager.py
+++ manager.py
@@ -34,8 +34,8 @@ def banner():
     for char in b:
         print(f'{random.choice(colors)}{char}{n}')
     print('Contact below address for get premium script')
-    print(f'{lg}Version: {w}2.0{lg} | GitHub: {w}@saifalisew1508{rs}')
-    print(f'{lg}Telegram: {w}@DearSaif{lg} | Instagram: {w}@_Prince.Babu_{rs}')
+    print(f'{lg}Version: {w}2.0{lg} | GitHub: {w}@saifalisew1508')
+    print(f'{lg}Telegram: {w}@DearSaif{lg} | Instagram: {w}@_Prince.Babu')
 
 def clr():
     if os.name == 'nt':
